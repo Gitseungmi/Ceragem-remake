b2f1cf091b0b9ef018f7357a658a0dd7642ac8c5
Gitseungmi <seungmin61119@gmail.com>
2024-11-07 10:21:03 +0900

    .

--- js/reaction.js
+++ js/reaction.js
@@ -1,17 +1,4 @@
 $(document).ready(function () {
-    // 드롭다운 토글
-    $(".relative a").click(function () {
-        $(this).siblings(".business-sub-list").slideToggle(200);
-    });
-
-    // 드롭다운 외부 클릭시 닫기
-    $(document).click(function (event) {
-        if (!$(event.target).closest(".relative").length) {
-            $(".business-sub-list").slideUp(200);
-        }
-    });
-});
-$(document).ready(function () {
     // 햄버거 메뉴 클릭
     $(".hamburger").on("click", function (e) {
         e.preventDefault();
